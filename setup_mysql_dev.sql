910578d89e51792e9f2133190122d424331c6da2
JonaNdawula <jonathan.ndawula@yahoo.com>
2024-01-15 18:17:27 -0800

    MySQL setup development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,5 @@
+-- Server for project
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PREVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@localhost';
