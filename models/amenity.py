b383f02276b3cd478bc8f82c80bdb01c7f75d962
1SkyShadow1 <Eazymoja5@gmail.com>
2024-01-17 16:49:57 -0800

    Correction of the Amenity file

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
+
+str_type = getenv("HBNB_TYPE_STORAGE")
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ document doc"""
+    __tablename__ = 'amenities'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        place-_amenities = relationship(
+            'Place', secondary='place_amenity',
+            back_populates="amenities")
+    else:
+        name = ""
