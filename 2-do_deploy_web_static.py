8a63e938f0fc22945c46c1794023331da37cb7d7
JonaNdawula <jonathan.ndawula@yahoo.com>
2024-02-01 09:55:04 -0800

    update for 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-A Fabric script that 
+A Fabric script that
 distributes an archive
 to my web servers
 """
@@ -8,7 +8,7 @@ import os
 from datetime import datetime
 from fabric.api import *
 
-env.hosts = ["100.25.4.139","54.90.24.175"]
+env.hosts = ["100.25.4.139", "54.90.24.175"]
 env.user = "ubuntu"
 
 
@@ -21,7 +21,7 @@ def do_pack():
     date_time = datetime.now().strftime("%Y%m%d%H%M%S")
     arch_file_path = f"versions/web_static_{date_time}.tgz"
     gzip_arch = local("tar -cvzf {} web_static".format(arch_file_path))
-    
+
     if gzip_arch.succeeded:
         return arch_file_path
     else:
@@ -34,11 +34,11 @@ def do_deploy(archive_path):
     """
     if os.path.exists(archive_path):
         file_arch = archive_path[9:]
-        new_version ="/data/web_static/releases/" + file_arch[:-4]
+        new_version = "/data/web_static/releases/" + file_arch[:-4]
         file_arch = "/tmp/" + file_arch
         put(archive_path, "/tmp/")
         run(f"sudo mkdir -p {new_version}")
-        run(f"sudo tar -xzf {file_arch} -C {new_version}")
+        run(f"sudo tar -xzf {file_arch} -C {new_version}/")
         run(f"sudo rm {file_arch}")
         run(f"sudo mv {new_version}/web_static/* {new_version}")
         run(f"sudo rm -rf {new_version}/web_static")
